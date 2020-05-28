8fc852395642640b33f24256f028e072e0255c58
Unknown <adamcantor22@gmail.com>
2020-05-28 17:35:04 -0400

    small padding change

--- tick_tok_bio/lib/main.dart
+++ tick_tok_bio/lib/main.dart
@@ -31,78 +31,82 @@ class _InputSectionState extends State<InputSection> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: myController,
-                decoration: InputDecoration(
-                  labelText: 'Test Input Here',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              RaisedButton(
-                onPressed: () {
-                  setState(
-                    () {
-                      if (_formKey.currentState.validate()) {
-                        String newUser = myController.text;
-                        Scaffold.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('submitting'),
-                          ),
-                        );
-                        Firestore.instance
-                            .collection('test_usernames')
-                            .document()
-                            .setData({'username': newUser});
-                      }
-                    },
-                  );
-                },
-                child: Text('Submit'),
-              ),
-            ],
-          ),
-        ),
-        RaisedButton(
-          onPressed: () {
-            setState(() {
-              showData = !showData;
-            });
-          },
-          child: Text('Toggle Showing Data'),
-        ),
-        if (showData)
-          StreamBuilder(
-            stream: Firestore.instance.collection('test_usernames').snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) return Text('Loading...');
-              return Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemExtent: 80.0,
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(
-                        snapshot.data.documents[index]['username'],
-                      ),
-                    );
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: <Widget>[
+          Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: myController,
+                  decoration: InputDecoration(
+                    labelText: 'Test Input Here',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
                   },
                 ),
-              );
-            },
+                RaisedButton(
+                  onPressed: () {
+                    setState(
+                      () {
+                        if (_formKey.currentState.validate()) {
+                          String newUser = myController.text;
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('submitting'),
+                            ),
+                          );
+                          Firestore.instance
+                              .collection('test_usernames')
+                              .document()
+                              .setData({'username': newUser});
+                        }
+                      },
+                    );
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
           ),
-      ],
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                showData = !showData;
+              });
+            },
+            child: Text('Toggle Showing Data'),
+          ),
+          if (showData)
+            StreamBuilder(
+              stream:
+                  Firestore.instance.collection('test_usernames').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return Text('Loading...');
+                return Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemExtent: 80.0,
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          snapshot.data.documents[index]['username'],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+        ],
+      ),
     );
   }
 }
