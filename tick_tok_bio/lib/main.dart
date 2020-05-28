dda4ed5de463c7ad861d77b027018994226cd5cd
Unknown <adamcantor22@gmail.com>
2020-05-28 17:31:19 -0400

    Firebase Demo
    We now have a simple app that successfully reads and writes a test property ('test_usernames') from and into a Firebase database.

--- tick_tok_bio/lib/main.dart
+++ tick_tok_bio/lib/main.dart
@@ -25,23 +25,84 @@ class InputSection extends StatefulWidget {
 }
 
 class _InputSectionState extends State<InputSection> {
+  final _formKey = GlobalKey<FormState>();
+  final myController = TextEditingController();
+  bool showData = false;
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            initialValue: 'Test Input Here',
+    return Column(
+      children: <Widget>[
+        Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: myController,
+                decoration: InputDecoration(
+                  labelText: 'Test Input Here',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              RaisedButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      if (_formKey.currentState.validate()) {
+                        String newUser = myController.text;
+                        Scaffold.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('submitting'),
+                          ),
+                        );
+                        Firestore.instance
+                            .collection('test_usernames')
+                            .document()
+                            .setData({'username': newUser});
+                      }
+                    },
+                  );
+                },
+                child: Text('Submit'),
+              ),
+            ],
           ),
-          RaisedButton(
-            onPressed: () {
-              //do something
-              return;
+        ),
+        RaisedButton(
+          onPressed: () {
+            setState(() {
+              showData = !showData;
+            });
+          },
+          child: Text('Toggle Showing Data'),
+        ),
+        if (showData)
+          StreamBuilder(
+            stream: Firestore.instance.collection('test_usernames').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return Text('Loading...');
+              return Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemExtent: 80.0,
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        snapshot.data.documents[index]['username'],
+                      ),
+                    );
+                  },
+                ),
+              );
             },
-            child: Text('Submit'),
           ),
-        ],
-      ),
+      ],
     );
   }
 }
