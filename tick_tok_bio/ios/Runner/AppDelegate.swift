2106059cee7fc314b4aa9e4c1387a74c3e3c727e
Unknown <adamcantor22@gmail.com>
2020-06-01 15:36:16 -0400

    Added Google Map support
    Does not include any changes to main

--- tick_tok_bio/ios/Runner/AppDelegate.swift
+++ tick_tok_bio/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCNb6xlu33o5wQpHNSYON-i3TxpShUMjGE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
