36a92547ed9db74c2ab8db82da676367fc3d489a
Unknown <adamcantor22@gmail.com>
2020-06-03 14:08:56 -0400

    Route Tracking active; Polyline auto-generated

--- tick_tok_bio/lib/gps_tracking.dart
+++ tick_tok_bio/lib/gps_tracking.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 //import 'package:gpx/gpx.dart';
 
 class Maps extends StatefulWidget {
@@ -17,11 +18,40 @@ class MapsState extends State<Maps> {
   static final initialPosition =
       CameraPosition(target: (LatLng(10.42, 16.45)), zoom: 18.0);
   GoogleMapController _controller;
-  List<LatLng> route;
+  LocationData currentLocation;
+  Location location;
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polyline> _polylines = Set<Polyline>();
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints;
+  bool trackingRoute = false;
 
   void startNewRoute() {
-    route = new List<LatLng>();
-    //route.add();
+    location = new Location();
+    polylinePoints = PolylinePoints();
+    trackingRoute = true;
+    location.onLocationChanged.listen((LocationData cLoc) {
+      currentLocation = cLoc;
+      LatLng pos =
+          new LatLng(currentLocation.latitude, currentLocation.longitude);
+      polylineCoordinates.add(pos);
+      debugPrint(polylineCoordinates.toString());
+      updatePolyline();
+    });
+  }
+
+  void updatePolyline() {
+    setState(() {
+      _polylines.add(
+        Polyline(
+          width: 5, // set the width of the polylines
+          polylineId: PolylineId('poly'),
+
+          color: Color.fromARGB(255, 40, 122, 198),
+          points: polylineCoordinates,
+        ),
+      );
+    });
   }
 
   @override
@@ -32,6 +62,9 @@ class MapsState extends State<Maps> {
       ),
       body: GoogleMap(
         myLocationEnabled: true,
+        compassEnabled: true,
+        markers: _markers,
+        polylines: _polylines,
         mapType: MapType.hybrid,
         initialCameraPosition: initialPosition,
         onMapCreated: (GoogleMapController controller) {
@@ -42,7 +75,9 @@ class MapsState extends State<Maps> {
         child: Icon(Icons.add),
         backgroundColor: Colors.blueAccent,
         onPressed: () {
-          startNewRoute();
+          if (!trackingRoute) {
+            startNewRoute();
+          }
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
