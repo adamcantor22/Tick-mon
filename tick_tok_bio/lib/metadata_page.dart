cfc84b86a08402c7f6a3ea254b2eca1c21b0d1a2
Unknown <adamcantor22@gmail.com>
2020-06-04 13:24:33 -0400

    No function change

--- tick_tok_bio/lib/metadata_page.dart
+++ tick_tok_bio/lib/metadata_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'main.dart';
 
 class MetadataSection extends StatefulWidget {
   const MetadataSection({Key key}) : super(key: key);
@@ -10,6 +11,13 @@ class MetadataSection extends StatefulWidget {
 class _MetadataSectionState extends State<MetadataSection> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: FlatButton(
+        color: Colors.red,
+        onPressed: () {
+          HomePage.changePageIndex(2);
+        },
+      ),
+    );
   }
 }
