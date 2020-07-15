d28898895d4c42d571a52684da91dc788f32b8ab
Unknown <adamcantor22@gmail.com>
2020-07-15 16:32:11 -0400

    mutable popups

--- tick_tok_bio/lib/helper.dart
+++ tick_tok_bio/lib/helper.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'gps_tracking.dart';
+import 'decorationInfo.dart';
 
 class Helper {
   //Returns an AlertDialog with a custom message and an OK button
@@ -60,18 +61,74 @@ class HelperText extends StatefulWidget {
 class _HelperTextState extends State<HelperText> {
   int segment;
   BuildContext cont;
-  List<TextFormField> fields;
-  List<TextEditingController> controllers = [TextEditingController()];
+  List<Widget> fieldRows;
+  List<TextEditingController> controllers;
+  List<DropdownMenuItem<String>> items;
+  List<String> dropdownItems = [
+    'Blacklegged',
+    'Nymph',
+  ];
 
   _HelperTextState(int segment, BuildContext cont) {
     this.segment = segment;
     this.cont = cont;
-    fields = [
-      TextFormField(
-        controller: controllers[0],
-        validator: (val) => valid(val),
-      )
-    ];
+  }
+
+  void initState() {
+    super.initState();
+    controllers = new List<TextEditingController>();
+    fieldRows = new List<Widget>();
+    items = dropdownItems.map<DropdownMenuItem<String>>((String value) {
+      return DropdownMenuItem<String>(
+        value: value,
+        child: Text(value),
+      );
+    }).toList();
+    newField();
+  }
+
+  void newField() {
+    setState(() {
+      controllers.add(new TextEditingController());
+    });
+
+    TextFormField tmpField = TextFormField(
+      decoration: kTextFieldDecoration,
+      controller: controllers[controllers.length - 1],
+      validator: (val) => valid(val),
+    );
+    DropdownButtonFormField tmpDrop = DropdownButtonFormField(
+      value: dropdownItems[0],
+      items: items,
+      onChanged: (val) {
+        print(val);
+      },
+    );
+    Row tmpRow = Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Flexible(
+          flex: 5,
+          child: tmpField,
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+        Flexible(
+          flex: 4,
+          child: tmpDrop,
+        ),
+      ],
+    );
+
+    setState(() {
+      fieldRows.add(
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child: tmpRow,
+        ),
+      );
+    });
   }
 
   String valid(String val) {
@@ -88,9 +145,20 @@ class _HelperTextState extends State<HelperText> {
       title: Text('Segment $segment Metadata'),
       content: Form(
         key: formKey,
-        child: Column(mainAxisSize: MainAxisSize.min, children: fields),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: fieldRows,
+        ),
       ),
       actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              newField();
+            });
+          },
+        ),
         FlatButton(
           onPressed: () {
             if (formKey.currentState.validate()) {
